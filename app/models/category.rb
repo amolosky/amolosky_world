e85e7e0d407d4b7bb6c79a57ab09a573c90a41eb
Vivek Sainanee <sainanee@cmu.edu>
2012-04-24 20:21:50 -0400

    heeeeeello

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,10 @@
 class Category < ActiveRecord::Base
   has_many :photos
+  has_many :articles
+  
+    # make sure required fields are present
+  validates_presence_of :title, :content
+
   
   scope :active, where('active = ?', true)
   scope :alphabetical, order('name')
